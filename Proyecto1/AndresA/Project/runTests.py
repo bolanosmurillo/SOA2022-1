26054338f794fd958ddd8fd792296550617e993e
Andres Artavia <aalopz02@gmail.com>
2022-04-11 16:04:46 -0600

    Update runTests.py

--- Proyecto1/AndresA/Project/runTests.py
+++ Proyecto1/AndresA/Project/runTests.py
@@ -5,12 +5,10 @@ import traceback
 
 try:
     main(mode="t")
-    exit(0)
+    print("0")
 except AssertionError:
     _, _, tb = sys.exc_info()
     traceback.print_tb(tb)
     tb_info = traceback.extract_tb(tb)
     filename, line, func, text = tb_info[-1]
-
     print('An error occurred on line {} in statement {}'.format(line, text))
-    exit(1)
