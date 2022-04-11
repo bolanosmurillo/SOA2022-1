40b534d995ffb44f926fa01833566f4f3da37f0c
Andres Artavia <aalopz02@gmail.com>
2022-04-11 16:01:52 -0600

    unit test phase

---
+++ Proyecto1/AndresA/Project/mainScript.py
@@ -0,0 +1,57 @@
+##visionapi@my-project-1535378363990.iam.gserviceaccount.com
+from __future__ import print_function
+from google.cloud import vision
+
+import platform
+import subprocess 
+
+import sys
+
+uri_base = ('alooo','eu.artifacts.my-project-1535378363990.appspot.com','gs://eu.artifacts.my-project-1535378363990.appspot.com')
+pic = ('face_surprise.jpg')
+
+
+def ping(host):
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    command = ['ping', param, '1', host]
+    return subprocess.call(command) == 0
+
+class interface():
+    def setImg(self,imgUrl=0):
+        pass
+    def doDetect(self):
+        pass
+    
+def getImpl(mode):
+    if (mode == 't'):
+        class test(interface):
+            def setImg(self,imgUrl=0):
+                assert ping(uri_base[0])
+            def doDetect(self):
+                testImgs = ('angerTest.jpg','happyTest.jpg')
+                client = vision.ImageAnnotatorClient()
+                image = vision.Image()
+                resultsTotal = []
+                for testImg in testImgs:
+                    image.source.image_uri = '%s/%s' % (uri_base[1], testImg)
+                    response = client.face_detection(image=image)
+                    results = {"joy":"","anger":""}
+                    for face in response.face_annotations:
+                        results["joy"] = vision.Likelihood(face.joy_likelihood).name
+                        results["anger"] = vision.Likelihood(face.anger_likelihood).name
+                    resultsTotal += [results]
+                testLabels = [{'joy': 'VERY_UNLIKELY', 'anger': 'VERY_LIKELY'}, {'joy': 'VERY_LIKELY', 'anger': 'VERY_UNLIKELY'}]
+                for i in range(len(resultsTotal)):
+                    assert resultsTotal[i]["joy"] == testLabels[i]["joy"]
+                    assert resultsTotal[i]["anger"] == testLabels[i]["anger"]
+        return test()
+    else:
+        pass
+
+
+def main(mode = 'r',imgB64 = 0):
+    run = 0
+    run = getImpl(mode)
+    run.setImg()
+    run.doDetect()
+
---
+++ Proyecto1/AndresA/Project/runTests.py
@@ -0,0 +1,16 @@
+from mainScript import main
+
+import sys
+import traceback
+
+try:
+    main(mode="t")
+    exit(0)
+except AssertionError:
+    _, _, tb = sys.exc_info()
+    traceback.print_tb(tb)
+    tb_info = traceback.extract_tb(tb)
+    filename, line, func, text = tb_info[-1]
+
+    print('An error occurred on line {} in statement {}'.format(line, text))
+    exit(1)
