1fb1d29ecbc68eac67d618997ae7036b6943950c
jasago <jasago0408@gmail.com>
2022-04-15 12:30:19 -0600

    Se agrego una prueba unitario de la funcion de deteccion de emociones

---
+++ Proyecto 1/proyecto1_jason_salazar/modules/cloud_function/test_function.py
@@ -0,0 +1,13 @@
+import emotion_detection
+
+def test_emo_detect():
+    
+    #public data
+    test_uri_base = 'cloud-vision-codelab'
+    test_pic = 'face_surprise.jpg'
+
+    result = emotion_detection.emo_detect(test_uri_base, test_pic)
+    
+    
+    assert result[0] == "Surprice"
+    assert result[1] == "LIKELY"
