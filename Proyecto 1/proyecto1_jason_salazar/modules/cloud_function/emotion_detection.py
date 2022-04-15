7a7fd1c1d7a58015ee8f0d7eabb2aa868ab54a38
jasago <jasago0408@gmail.com>
2022-04-15 13:21:12 -0600

    Se realiza mejoras en el formato del codigo py

--- Proyecto 1/proyecto1_jason_salazar/modules/cloud_function/emotion_detection.py
+++ Proyecto 1/proyecto1_jason_salazar/modules/cloud_function/emotion_detection.py
@@ -5,11 +5,11 @@ from google.cloud import vision
 def get_most_likely(emo, likelihood, most_likely_emotion):
     """Dado dos emociones conserva la de mayor probabilidad"""
     likelihood = likelihood.replace('Likelihood.', '')
-    if( likelihood == "VERY_LIKELY"):
+    if likelihood == "VERY_LIKELY":
         return (emo,likelihood)
-    elif (likelihood == "LIKELY" and most_likely_emotion[1] != "VERY_LIKELY" ):
+    if (likelihood == "LIKELY" and most_likely_emotion[1] != "VERY_LIKELY" ):
         return (emo,likelihood)
-    elif  (likelihood == "POSSIBLE" and most_likely_emotion[1] == "-" ):
+    if  (likelihood == "POSSIBLE" and most_likely_emotion[1] == "-" ):
         return (emo,likelihood)
     else:
         return most_likely_emotion
@@ -19,7 +19,8 @@ def get_emotion(face):
     most_likely_emotion = ("Ninguna","-")
     most_likely_emotion =  get_most_likely("Anger", str(face.anger_likelihood), most_likely_emotion)
     most_likely_emotion =  get_most_likely("Joy", str(face.joy_likelihood), most_likely_emotion)
-    most_likely_emotion =  get_most_likely("Surprice", str(face.surprise_likelihood), most_likely_emotion)
+    most_likely_emotion =  get_most_likely("Surprice", str(face.surprise_likelihood),
+    most_likely_emotion)
 
     return most_likely_emotion
 
