de7ab0a887a836b2c3734f1fb88b37051bcab607
jasago <jasago0408@gmail.com>
2022-04-15 13:15:05 -0600

    Compropbando el funcionamiento de la etapa lint del pipeline 3

--- Proyecto 1/proyecto1_jason_salazar/modules/cloud_function/emotion_detection.py
+++ Proyecto 1/proyecto1_jason_salazar/modules/cloud_function/emotion_detection.py
@@ -1,7 +1,9 @@
-from __future__ import print_function
+##from __future__ import print_function
+"""Modulo necesario para utiliza Google Vsion API"""
 from google.cloud import vision
 
 def get_most_likely(emo, likelihood, most_likely_emotion):
+    """Dado dos emociones conserva la de mayor probabilidad"""
     likelihood = likelihood.replace('Likelihood.', '')
     if( likelihood == "VERY_LIKELY"):
         return (emo,likelihood)
@@ -13,16 +15,16 @@ def get_most_likely(emo, likelihood, most_likely_emotion):
         return most_likely_emotion
 
 def get_emotion(face):
-
-    most_likely_emotion = ("Ninguna","-")        
+    """Permite seleccionar la emocion mas probable"""
+    most_likely_emotion = ("Ninguna","-")
     most_likely_emotion =  get_most_likely("Anger", str(face.anger_likelihood), most_likely_emotion)
     most_likely_emotion =  get_most_likely("Joy", str(face.joy_likelihood), most_likely_emotion)
     most_likely_emotion =  get_most_likely("Surprice", str(face.surprise_likelihood), most_likely_emotion)
-    
+
     return most_likely_emotion
 
 def emo_detect(uri_base, pic):
-
+    """Permite detectar la emocion de un rosto en una imagen"""
     client = vision.ImageAnnotatorClient()
     image = vision.Image()
     image.source.image_uri = 'gs://%s/%s' % (uri_base, pic)
