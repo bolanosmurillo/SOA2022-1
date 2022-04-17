ed9e381c41e901022d7da4e2aba24b564ddeeb0b
Andres Artavia <aalopz02@gmail.com>
2022-04-16 22:05:15 -0600

    path test

--- Proyecto1/AndresA/Project/terraform/cloud_function.tf
+++ Proyecto1/AndresA/Project/terraform/cloud_function.tf
@@ -1,7 +1,7 @@
 data "archive_file" "source"{
     type = "zip"
-    source_dir = "${path.module}/cloud_function"
-    output_path = "${path.module}/cloud_function/cloud_function.zip"
+    source_dir = "${path.module}/"
+    output_path = "${path.module}/cloud_function.zip"
 }
 
 resource "google_storage_bucket_object" "zip" {
