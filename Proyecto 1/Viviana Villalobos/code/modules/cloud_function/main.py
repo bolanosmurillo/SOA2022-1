6efe212cfe74f3062c13ab6fb3a2bf6e7cbdadcc
Viviana <vvillalobosv@estudiantec.cr>
2022-04-19 08:09:28 -0600

    delete doc string

--- Proyecto 1/Viviana Villalobos/code/modules/cloud_function/main.py
+++ Proyecto 1/Viviana Villalobos/code/modules/cloud_function/main.py
@@ -4,9 +4,6 @@ Main funct that called the emotion detection one
 import emotion_detection
 
 def main(event):
-    """
-    Main that calls with the image
-    """
     image = event['name']        #Nombre de la imagen a analizar
 
     result = emotion_detection.get_emotion(image) #Detectar emocion
