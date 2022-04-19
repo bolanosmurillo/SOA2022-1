da77af8c2ee02257d4caa969889fcc4baca9754c
adrian <56691299+AdrianTEC@users.noreply.github.com>
2022-04-19 02:05:45 -0600

    asdasd

--- Proyecto1/terraform/environments/main/bucket.tf
+++ Proyecto1/terraform/environments/main/bucket.tf
@@ -1,8 +1,9 @@
 resource "google_storage_bucket" "function_bucket" {
-    name = "${var.project_id}-function"
+    name     = "${var.project_id}-function-test"
     location = var.region
 }
-resource "google_storage_bucket" "input" {
-    name = "${var.project_id}-input"
+
+resource "google_storage_bucket" "input_bucket" {
+    name     = "${var.project_id}-input-test"
     location = var.region
 }
