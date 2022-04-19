50d1aaf19bd6750ab32766af338735aca765de72
Viviana <vvillalobosv@estudiantec.cr>
2022-04-19 07:04:09 -0600

    Update test_func.py

--- Proyecto 1/Viviana Villalobos/code/modules/cloud_function/test_func.py
+++ Proyecto 1/Viviana Villalobos/code/modules/cloud_function/test_func.py
@@ -2,7 +2,7 @@ import emotion_detection
 
 def test_get_emotion():
 
-    result = emotion_detection.get_emotion("persona1.jpg")
+    result = emotion_detection.get_emotion('persona1.jpg')
 
 
     assert result[0] == "Joy"
