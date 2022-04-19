28b842fb3cb2c7cc8c5e903176fc72b0a93bab4b
adrian <56691299+AdrianTEC@users.noreply.github.com>
2022-04-19 02:22:18 -0600

    r

--- Proyecto1/terraform/environments/main/bucket.tf
+++ Proyecto1/terraform/environments/main/bucket.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "function_bucket" {
     name     = "${var.project_id}-function-test"
-    location = "europe-west1"
+    location = var.region
 }
 
 resource "google_storage_bucket" "input_bucket" {
     name     = "${var.project_id}-input-test"
-    location = "europe-west1"
+    location = var.region
 }
