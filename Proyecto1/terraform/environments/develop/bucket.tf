0151fe9377e6368b544912eadf9407d3c1d7b0da
Sebastian <scyock.03@gmail.com>
2022-04-19 09:42:29 -0600

    Commit after time limit

--- Proyecto1/terraform/environments/develop/bucket.tf
+++ Proyecto1/terraform/environments/develop/bucket.tf
@@ -1,11 +1,11 @@
 resource "google_storage_bucket" "function_bucket" {
     name     = "${var.project}-function-prod"
-    location = var.region
+    location = "global"
     force_destroy = true
 }
 
 resource "google_storage_bucket" "input_bucket" {
     name     = "${var.project}-input-prod"
-    location = var.region
+    location = "global"
     force_destroy = true
 }
