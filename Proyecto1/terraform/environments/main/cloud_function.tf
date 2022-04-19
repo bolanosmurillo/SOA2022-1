9b16731687be56fc0b712052dde979fd5d1324e1
adrian <56691299+AdrianTEC@users.noreply.github.com>
2022-04-19 02:36:48 -0600

    sd

--- Proyecto1/terraform/environments/main/cloud_function.tf
+++ Proyecto1/terraform/environments/main/cloud_function.tf
@@ -27,7 +27,7 @@ resource "google_cloudfunctions_function" "function" {
 
     event_trigger {
         event_type = "google.storage.object.finalize"
-        resource   = "${var.project_id}-input"
+        resource   = "soa1"
     }
 
 }
