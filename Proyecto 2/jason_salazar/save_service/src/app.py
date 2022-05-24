60abe4152803725e77afd537bd8dda855ef095d8
jasago <jasago0408@gmail.com>
2022-05-24 08:54:43 -0600

    Se agrega modulo write fiel en save service

--- Proyecto 2/jason_salazar/save_service/src/app.py
+++ Proyecto 2/jason_salazar/save_service/src/app.py
@@ -1,23 +1,21 @@
 """Este modulo permite guardar los resultados del analisis en un archivo compartido"""
 import os
 import pika
-
+import write_file
 rabbit_host = os.getenv("RABBIT_HOST")
 rabbit_port = os.getenv("RABBIT_PORT")
 rabbit_user = os.getenv("RABBIT_USERNAME")
 rabbit_password = os.getenv("RABBIT_PASSWORD")
 
 def on_message_received(cha, method, properties, body):
-    """Permite escribir el mensaje en un archivo de salida"""
+    """Permite mandar a escribir el mensaje en un archivo de salida"""
     message = f"{body.decode()}"
 
-    with open("../host/output.txt", "a", encoding="utf8") as output:
-        output.write(message+'\n')
+    result = write_file.write_save("../host/output.txt", message)
 
-    output.close()
-
-    #Indicar a la cola que el mesaje ya fue procesado
-    cha.basic_ack(delivery_tag = method.delivery_tag)
+    if result == 1:
+        #Indicar a la cola que el mesaje ya fue procesado
+        cha.basic_ack(delivery_tag = method.delivery_tag)
 
 #Se establecen los parametros para la conexion
 credentials = pika.PlainCredentials(rabbit_user, rabbit_password)
