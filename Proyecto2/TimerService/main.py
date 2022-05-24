60b1d1806e11134b952f290cacc7c7a29bf92e48
adrian <56691299+AdrianTEC@users.noreply.github.com>
2022-05-24 10:43:03 -0600

    fixing running on docker

--- Proyecto2/TimerService/main.py
+++ Proyecto2/TimerService/main.py
@@ -3,24 +3,25 @@ import datetime
 import pytz
 import pika
 
+
 def get_time():
     """
     DEF:
         Se encarga de activar el proceso de obtencion de las imagenes una vez la hora se cumpla
 
     """
-    get_images = False
-    hour=9
+    hour=10
+    mesagge_sent=False
     while True:
         fecha = datetime.datetime.now(tz=pytz.timezone('America/Costa_Rica'))
         print("target:"+ str(hour))
         print("current:"+ str(fecha.hour))
-        if fecha.hour == hour and not get_images:
-            get_images = True
+        if fecha.hour == hour and not mesagge_sent:
+            mesagge_sent = True
             send_signal()
             break
         if fecha.hour != hour:
-            get_images = False
+            mesagge_sent = False
 
 def send_signal():
     """
@@ -34,5 +35,4 @@ def send_signal():
                       body="Time!")
     print(" TIMER_SERVICE: Mensaje Enviado")
     connection.close()
-
 get_time()
